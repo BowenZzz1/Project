d86c2eac3bd6b49ac7e30cafc6d917b892015f62
BowenZzz1 <77997193+BowenZzz1@users.noreply.github.com>
2024-06-24 12:02:30 -0700

    Add files via upload

--- weather/search/src/main/java/com/example/search/service/SearchService.java
+++ weather/search/src/main/java/com/example/search/service/SearchService.java
@@ -1,6 +1,6 @@
 package com.example.search.service;
 
-import com.example.common.domain.GeneralResponse;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
